128d37f8d1c292d67db7080d0193a117d1fe9f36
vCra <aaron+github@vcra.io>
2019-09-10 15:14:56 +0100

    Enable debug

--- mealdeal/mealdeal/settings.py
+++ mealdeal/mealdeal/settings.py
@@ -15,6 +15,7 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+DEBUG = os.environ.get('DJANGO_DEBUG', False)
 
 
 # Application definition
@@ -61,6 +62,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
 
 
 # Static files (CSS, JavaScript, Images)
